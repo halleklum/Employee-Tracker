5b7bc90d84848a58cde7e4442332f3ed4ea9a35f
halleklum <halleklum@gmail.com>
2023-07-30 18:02:19 -0400

    added cfonts / embellished start page

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cfonts = require('cfonts');
 
+cfonts.say('BARBIE Movie \nEmployee \nTracker', {
+	font: 'block',              // define the font face
+	align: 'left',              // define text alignment
+	colors: ['magentaBright'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: true,            // define your two gradient colors
+	independentGradient: true, // define if you want to recalculate the gradient for each new line
+	transitionGradient: true,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
+});
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -137,7 +152,7 @@ function addDepartment() {
             type: "list",
             name: "department",
             message: "Which department is this role in?",
-            choices: departmentChoices,
+            choices: "",
           },
         ])
         .then((answers) => {
